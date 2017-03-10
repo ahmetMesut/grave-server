91941b74b3127e03ef3707bce3bf92718780b647
Ahmet Mesut Şahinoğlu <ahmet.sahinoglu@mebitech.com>
2017-03-10 16:20:13 +0300

    fixed.

--- src/main/java/com/mebitech/grave/GraveApplication.java
+++ src/main/java/com/mebitech/grave/GraveApplication.java
@@ -2,6 +2,7 @@ package com.mebitech.grave;
 
 import com.mebitech.grave.cli.GraveInitializeCommand;
 import com.mebitech.grave.conf.GraveConfiguration;
+import io.dropwizard.servlets.CacheBustingFilter;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
 import io.robe.admin.RobeApplication;
@@ -39,9 +40,7 @@ public class GraveApplication extends RobeApplication<GraveConfiguration> {
         super.run(configuration, environment);
 
         // Enable CORS headers
-        final FilterRegistration.Dynamic cors =
-                environment.servlets()
-                        .addFilter("CORS", CrossOriginFilter.class);
+        final FilterRegistration.Dynamic cors = environment.servlets().addFilter("CORS", CrossOriginFilter.class);
 
         // Configure CORS parameters
         cors.setInitParameter("allowedOrigins", "*");
@@ -52,5 +51,7 @@ public class GraveApplication extends RobeApplication<GraveConfiguration> {
         // Add URL mapping
         cors.addMappingForUrlPatterns(EnumSet.allOf(DispatcherType.class), true, "/*");
 
+        environment.servlets().addFilter("CacheBustingFilter", new CacheBustingFilter())
+                .addMappingForUrlPatterns(EnumSet.of(DispatcherType.REQUEST), true, "/*");
     }
 }
